e107481da647696b7fe3c2ed78886061ad483de5
websolutionstuff <websolutionstuff@gmail.com>
2020-08-16 11:52:16 +0530

    Laravel REST API Tutorial

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Product;
+use Validator;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Resources\Product as ProductResource;
+
+class ProductController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return $this->sendResponse(ProductResource::collection($products), 'Products Retrieved Successfully.');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+   
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $product = Product::create($input);
+   
+        return $this->sendResponse(new ProductResource($product), 'Product Created Successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+  
+        if (is_null($product)) {
+            return $this->sendError('Product not found.');
+        }
+   
+        return $this->sendResponse(new ProductResource($product), 'Product Retrieved Successfully.');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $input = $request->all();
+   
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+        $product = Product::find($id);   
+        $product->name = $input['name'];
+        $product->detail = $input['detail'];
+        $product->save();
+   
+        return $this->sendResponse(new ProductResource($product), 'Product Updated Successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+   
+        return $this->sendResponse([], 'Product Deleted Successfully.');
+    }
+}
